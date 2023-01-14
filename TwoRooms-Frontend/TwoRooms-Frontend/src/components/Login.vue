03bfadb429137c47caa5b2f688652f07b8ab5fe2
Ryan Lake <rcl.catcher@gmail.com>
2023-01-13 23:15:16 -0700

    Initial Commit

---
+++ TwoRooms-Frontend/TwoRooms-Frontend/src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+   <input type="text" class="form-control m-2" v-model="accountName" placeholder="Email" />
+   <input type="password" class="form-control m-2" v-model="accountPassword" placeholder="Password" />
+
+   <div class="d-flex justify-content-center">
+      <button type="button" class="btn btn-outline-dark" v-on:click="login">Log In</button>
+   </div>
+</template>
+
+<script>
+   export default {
+      name: 'Login',
+      data() {
+         return {
+            accountName: '',
+            accountPassword: ''
+         }
+      },
+      methods: {
+         login() {
+            console.log(`logging in for account '${this.accountName}' and password '${this.accountPassword}'`)
+
+            fetch("http://localhost:8080/api/game/23?action=viewPlayers", { mode: 'no-cors' })
+               .then(resp => {
+                  console.log('resp', resp);
+                  return resp.json();
+               })
+               .then(apiObj => {
+                  console.log('apiObj', apiObj)
+               });
+
+         }
+      }
+   }
+</script>
+
+<style scoped>
+</style>
